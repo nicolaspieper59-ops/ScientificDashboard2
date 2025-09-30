beb452db56eb94513e0e59c9b95882c10605def3
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-30 10:34:32 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'scientific-dashboard-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './sw-register.js'
-];
-
-// Installer le service worker et mettre en cache les fichiers
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open('vitesse-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './manifest.json',
+        './js/compteurVitesse.js'
+      ]);
+    })
   );
 });
 
-// Activer le service worker et nettoyer les anciennes caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Intercepter les requ¨ºtes et renvoyer depuis le cache si hors-ligne
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request).catch(() => 
-        new Response('Ressource non disponible hors-ligne.', {status:404})
-      ))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
