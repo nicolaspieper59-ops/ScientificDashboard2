44c5630f86cd23c5a41ba7ca4efb0ed55b47b7df
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-30 11:13:29 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('vitesse-cache').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './manifest.json',
-        './js/compteurVitesse.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(() => {
+    console.log("🛰️ Mode hors ligne activé");
+  });
+}
