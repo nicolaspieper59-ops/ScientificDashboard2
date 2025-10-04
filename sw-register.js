e66061b2af8bbb006076f356a17285e89ccafb04
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-10-04 16:09:02 +0000

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,1 @@
-<script>
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('SW enregistré avec succès:', reg))
-    .catch(err => console.error('Échec enregistrement SW:', err));
-}
-</script>
+ 
