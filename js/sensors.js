b5db0b993184ab4113958505b7ee7dc9452c50e6
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-10-07 09:21:46 +0000

    Update sensors.js

--- js/sensors.js
+++ js/sensors.js
@@ -1,20 +1,76 @@
-// Capteurs : accélération Z, baromètre, lumière, son, magnétomètre
 const Sensors = (function(){
-  let accelZ = 0;
+  let accel = {x:0,y:0,z:0};
+  let light=0, sound=0, magnet=0, pressure=0, ultraSon=0, infraRouge=0;
+
   function startAccelerometer(){
     if(window.DeviceMotionEvent){
       window.addEventListener('devicemotion', e=>{
-        accelZ = e.accelerationIncludingGravity?.z || 0;
+        accel.x = e.accelerationIncludingGravity?.x || 0;
+        accel.y = e.accelerationIncludingGravity?.y || 0;
+        accel.z = e.accelerationIncludingGravity?.z || 0;
       });
     }
   }
-  function getAccelZ(){ return accelZ; }
 
-  // Placeholder pour autres capteurs
-  let light=0, sound=0, magnet=0, pressure=0;
-  function startOtherSensors(){
-    // TODO : baromètre, luminosité, son, magnétomètre
+  function startLightSensor(){
+    if('AmbientLightSensor' in window){
+      try{
+        const sensor = new AmbientLightSensor();
+        sensor.onreading = ()=> light = sensor.illuminance;
+        sensor.start();
+      } catch(e){ console.warn('Light sensor non dispo'); }
+    }
   }
 
-  return {startAccelerometer,getAccelZ,startOtherSensors};
+  function startMagnetometer(){
+    if('Magnetometer' in window){
+      try{
+        const sensor = new Magnetometer();
+        sensor.onreading = ()=> magnet = sensor.x; 
+        sensor.start();
+      } catch(e){ console.warn('Magnétomètre non dispo'); }
+    }
+  }
+
+  function startBarometer(){
+    if('PressureSensor' in window){
+      try{
+        const sensor = new PressureSensor();
+        sensor.onreading = ()=> pressure = sensor.pressure;
+        sensor.start();
+      } catch(e){ console.warn('Baromètre non dispo'); }
+    }
+  }
+
+  function startMicrophone(){
+    navigator.mediaDevices.getUserMedia({audio:true}).then(stream=>{
+      const audioCtx = new (window.AudioContext||window.webkitAudioContext)();
+      const source = audioCtx.createMediaStreamSource(stream);
+      const analyser = audioCtx.createAnalyser();
+      source.connect(analyser);
+      const data = new Uint8Array(analyser.frequencyBinCount);
+      setInterval(()=>{ analyser.getByteTimeDomainData(data); sound = Math.max(...data); }, 200);
+    }).catch(()=>{ console.warn('Microphone non dispo'); });
+  }
+
+  // Placeholder : infra-rouge et ultra-son via Bluetooth ou capteurs spéciaux
+  function startInfraUltrasons(){ infraRouge=0; ultraSon=0; }
+
+  function startAll(){
+    startAccelerometer();
+    startLightSensor();
+    startMagnetometer();
+    startBarometer();
+    startMicrophone();
+    startInfraUltrasons();
+  }
+
+  function getAll(){
+    return {
+      accel, light, sound, magnet, pressure, infraRouge, ultraSon
+    };
+  }
+
+  return {startAll, getAll};
 })();
+    
