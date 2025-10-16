bb9f12267655272521faef5d8e55e6912397a9fb
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-10-16 14:56:52 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
- 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('dashboard-cache-v1').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/cockpit.js',
+      '/manifest.json',
+      '/styles.css',
+      // Ajoutez ici d'autres fichiers à mettre en cache
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+}); 
